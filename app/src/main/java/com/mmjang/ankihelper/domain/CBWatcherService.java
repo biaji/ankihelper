d05481ce5034ad4ab4eb029750e746d4c609521b
biaji <biaji@biaji.net>
2026-09-24 16:57:58 +0800

    1. 高版本适配：去除高版本Android external权限申请 2. 移除首页打赏 qq群条目 3. 版本升了一点点

--- app/src/main/java/com/mmjang/ankihelper/domain/CBWatcherService.java
+++ app/src/main/java/com/mmjang/ankihelper/domain/CBWatcherService.java
@@ -1,7 +1,5 @@
 package com.mmjang.ankihelper.domain;
 
-
-
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
--- app/src/main/java/com/mmjang/ankihelper/ui/LauncherActivity.java
+++ app/src/main/java/com/mmjang/ankihelper/ui/LauncherActivity.java
@@ -1,5 +1,6 @@
 package com.mmjang.ankihelper.ui;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -31,7 +32,6 @@ import com.mmjang.ankihelper.data.plan.OutputPlanPOJO;
 import com.mmjang.ankihelper.domain.CBWatcherService;
 import com.mmjang.ankihelper.MyApplication;
 import com.mmjang.ankihelper.data.Settings;
-import com.mmjang.ankihelper.ui.about.AboutActivity;
 import com.mmjang.ankihelper.ui.content.ContentActivity;
 import com.mmjang.ankihelper.ui.customdict.CustomDictionaryActivity;
 import com.mmjang.ankihelper.ui.plan.PlansManagerActivity;
@@ -53,10 +53,8 @@ public class LauncherActivity extends AppCompatActivity {
     Switch switchPinkTheme;
     TextView textViewOpenPlanManager;
     TextView textViewCustomDictionary;
-    TextView textViewAbout;
     TextView textViewHelp;
     TextView textViewAddDefaultPlan;
-    TextView textViewAddQQGroup;
     TextView textViewRandomQuote;
     TextView textViewCustomTranslation;
 
@@ -65,8 +63,8 @@ public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        settings = Settings.getInstance(this);
         if (settings.getPinkThemeQ()) {
+        settings = Settings.getInstance(this);
             setTheme(R.style.AppThemePink);
         }
         super.onCreate(savedInstanceState);
@@ -79,13 +77,10 @@ public class LauncherActivity extends AppCompatActivity {
         switchPinkTheme = (Switch) findViewById(R.id.pink_theme_switch);
         textViewOpenPlanManager = (TextView) findViewById(R.id.btn_open_plan_manager);
         textViewCustomDictionary = (TextView) findViewById(R.id.btn_open_custom_dictionary);
-        textViewAbout = (TextView) findViewById(R.id.btn_about_and_support);
         textViewHelp = (TextView) findViewById(R.id.btn_help);
         textViewAddDefaultPlan = (TextView) findViewById(R.id.btn_add_default_plan);
-        textViewAddQQGroup = (TextView) findViewById(R.id.btn_qq_group);
         textViewRandomQuote = (TextView) findViewById(R.id.btn_show_random_content);
         textViewCustomTranslation = findViewById(R.id.btn_set_custom_fanyi);
-        textViewAbout.setText(Html.fromHtml("<font color='red'>❤</font>" + getResources().getString(R.string.btn_about_and_support_str)));
         switchMoniteClipboard.setChecked(
                 settings.getMoniteClipboardQ()
         );
@@ -103,144 +98,96 @@ public class LauncherActivity extends AppCompatActivity {
         );
 
         switchMoniteClipboard.setOnCheckedChangeListener(
-                new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        settings.setMoniteClipboardQ(isChecked);
-                        if (isChecked) {
-                            startCBService();
-                        } else {
-                            stopCBService();
-                        }
+                (buttonView, isChecked) -> {
+                    settings.setMoniteClipboardQ(isChecked);
+                    if (isChecked) {
+                        startCBService();
+                    } else {
+                        stopCBService();
                     }
                 }
         );
 
         switchLeftHandMode.setOnCheckedChangeListener(
-                new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        settings.setLeftHandModeQ(isChecked);
-                    }
-                }
+                (buttonView, isChecked) -> settings.setLeftHandModeQ(isChecked)
         );
 
         switchCancelAfterAdd.setOnCheckedChangeListener(
-                new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        settings.setAutoCancelPopupQ(isChecked);
-                    }
-                }
+                (buttonView, isChecked) -> settings.setAutoCancelPopupQ(isChecked)
         );
 
         switchPinkTheme.setOnCheckedChangeListener(
-                new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                        settings.setPinkThemeQ(b);
-                        recreate();
-                    }
+                (compoundButton, b) -> {
+                    settings.setPinkThemeQ(b);
+                    recreate();
                 }
         );
 
         textViewOpenPlanManager.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (mAnkiDroid == null) {
-                            mAnkiDroid = new AnkiDroidHelper(LauncherActivity.this);
-                        }
-                        if (!AnkiDroidHelper.isApiAvailable(MyApplication.getContext())) {
-                            Toast.makeText(LauncherActivity.this, R.string.api_not_available_message, Toast.LENGTH_LONG).show();
-                            return;
-                        }
-
-                        if (mAnkiDroid.shouldRequestPermission()) {
-                            mAnkiDroid.requestPermission(LauncherActivity.this, 0);
-                            return;
-                        } else {
-
-                        }
-                        Intent intent = new Intent(LauncherActivity.this, PlansManagerActivity.class);
-                        startActivity(intent);
+                v -> {
+                    if (mAnkiDroid == null) {
+                        mAnkiDroid = new AnkiDroidHelper(LauncherActivity.this);
                     }
+                    if (!AnkiDroidHelper.isApiAvailable(MyApplication.getContext())) {
+                        Toast.makeText(LauncherActivity.this, R.string.api_not_available_message, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    if (mAnkiDroid.shouldRequestPermission()) {
+                        mAnkiDroid.requestPermission(LauncherActivity.this, 0);
+                        return;
+                    } else {
+
+                    }
+                    Intent intent = new Intent(LauncherActivity.this, PlansManagerActivity.class);
+                    startActivity(intent);
                 }
         );
 
         textViewCustomDictionary.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(LauncherActivity.this, CustomDictionaryActivity.class);
-                        startActivity(intent);
-                    }
-                }
-        );
-
-        textViewAbout.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(LauncherActivity.this, AboutActivity.class);
-                        startActivity(intent);
-                    }
+                v -> {
+                    Intent intent = new Intent(LauncherActivity.this, CustomDictionaryActivity.class);
+                    startActivity(intent);
                 }
         );
 
         textViewCustomTranslation.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(LauncherActivity.this, CustomTranslationActivity.class);
-                        startActivity(intent);
-                    }
+                view -> {
+                    Intent intent = new Intent(LauncherActivity.this, CustomTranslationActivity.class);
+                    startActivity(intent);
                 }
         );
 
         textViewHelp.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String url = "https://github.com/mmjang/ankihelper/blob/master/README.md";
-                        Intent i = new Intent(Intent.ACTION_VIEW);
-                        i.setData(Uri.parse(url));
-                        startActivity(i);
-                    }
+                v -> {
+                    String url = "https://github.com/mmjang/ankihelper/blob/master/README.md";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
                 }
         );
 
         textViewAddDefaultPlan.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (mAnkiDroid == null) {
-                            mAnkiDroid = new AnkiDroidHelper(LauncherActivity.this);
-                        }
-                        if (!AnkiDroidHelper.isApiAvailable(MyApplication.getContext())) {
-                            Toast.makeText(LauncherActivity.this, R.string.api_not_available_message, Toast.LENGTH_LONG).show();
-                            return;
-                        }
-
-                        if (mAnkiDroid.shouldRequestPermission()) {
-                            mAnkiDroid.requestPermission(LauncherActivity.this, 0);
-                            return;
-                        } else {
-
-                        }
-                        askIfAddDefaultPlan();
+                v -> {
+                    if (mAnkiDroid == null) {
+                        mAnkiDroid = new AnkiDroidHelper(LauncherActivity.this);
                     }
+                    if (!AnkiDroidHelper.isApiAvailable(MyApplication.getContext())) {
+                        Toast.makeText(LauncherActivity.this, R.string.api_not_available_message, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    if (mAnkiDroid.shouldRequestPermission()) {
+                        mAnkiDroid.requestPermission(LauncherActivity.this, 0);
+                        return;
+                    } else {
+
+                    }
+                    askIfAddDefaultPlan();
                 }
         );
 
-        textViewAddQQGroup.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        joinQQGroup("-1JxtFYckXpYUMpZKRbrMWuceCgM23R7");
-                    }
-                }
-        );
+
         if (settings.getMoniteClipboardQ()) {
             startCBService();
         }
@@ -254,20 +201,6 @@ public class LauncherActivity extends AppCompatActivity {
                     }
                 }
         );
-
-        //debug new feature
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    YoudaoOnline.getDefinition("dedicate");
-//                }
-//                catch (IOException e){
-//
-//                }
-//            }
-//        });
-//        thread.start();
     }
 
     private void checkAndRequestPermissions() {
@@ -276,6 +209,8 @@ public class LauncherActivity extends AppCompatActivity {
         }
         if (!AnkiDroidHelper.isApiAvailable(MyApplication.getContext())) {
             Toast.makeText(this, R.string.api_not_available_message, Toast.LENGTH_LONG).show();
+            initStoragePermission();
+            return;
         }
 
         if (mAnkiDroid.shouldRequestPermission()) {
@@ -310,27 +245,25 @@ public class LauncherActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults.length == 0) {
             return;
         }
 
         if (requestCode == REQUEST_CODE_ANKI) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 initStoragePermission();
             } else {
-                //Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_LONG).show();
                 new AlertDialog.Builder(LauncherActivity.this)
                         .setMessage(R.string.permission_denied)
                         .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                openSettingsPage();
-                            }
-                        }).show();
+                        .setPositiveButton(android.R.string.yes, (dialog, whichButton) -> openSettingsPage())
+                        .setOnDismissListener(dialog -> initStoragePermission())
+                        .show();
             }
         }
         if (requestCode == REQUEST_CODE_STORAGE) {
-            if (requestCode == REQUEST_CODE_STORAGE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 ensureExternalDbDirectoryAndMigrate();
                 askIfAddDefaultPlan();
             } else {
@@ -407,9 +340,11 @@ public class LauncherActivity extends AppCompatActivity {
                             try {
                                 DefaultPlan plan = new DefaultPlan(LauncherActivity.this);
                                 plan.addDefaultPlan();
-                                Toast.makeText(LauncherActivity.this, R.string.default_plan_added, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LauncherActivity.this, 
+                                        R.string.default_plan_added, Toast.LENGTH_SHORT).show();
                             }catch (Exception e){
-                                Toast.makeText(LauncherActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LauncherActivity.this, 
+                                        e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
                     })
@@ -430,26 +365,7 @@ public class LauncherActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /****************
-     *
-     * 发起添加群流程。群号：安卓划词助手用户群(871406754) 的 key 为： -1JxtFYckXpYUMpZKRbrMWuceCgM23R7
-     * 调用 joinQQGroup(-1JxtFYckXpYUMpZKRbrMWuceCgM23R7) 即可发起手Q客户端申请加群 安卓划词助手用户群(871406754)
-     *
-     * @param key 由官网生成的key
-     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
-     ******************/
-    public boolean joinQQGroup(String key) {
-        Intent intent = new Intent();
-        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
-        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        try {
-            startActivity(intent);
-            return true;
-        } catch (Exception e) {
-            // 未安装手Q或安装的版本不支持
-            return false;
-        }
-    }
+
 
     public void setVersion() {
         try {
@@ -464,16 +380,18 @@ public class LauncherActivity extends AppCompatActivity {
     }
 
     private void initStoragePermission() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            int result = ContextCompat.checkSelfPermission(LauncherActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (Build.VERSION.SDK_INT >= 23 && Build.VERSION.SDK_INT < 33) {
+            int result = ContextCompat.checkSelfPermission(LauncherActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
             if (result != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(LauncherActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                } else {
-                    ActivityCompat.requestPermissions(LauncherActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE);
-                }
+                ActivityCompat.requestPermissions(LauncherActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_STORAGE);
             } else {
                 ensureExternalDbDirectoryAndMigrate();
             }
+        } else {
+            ensureExternalDbDirectoryAndMigrate();
         }
     }
 }
